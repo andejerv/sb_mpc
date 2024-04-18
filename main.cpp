fc694b36ff8f007b8d763b92e4f1a05a75e44b1b
andejerv <jervananders@outlook.com>
2024-04-18 18:44:41 +0200

    Object class test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cmath>
+
+#include "object.h"
+#include "utilities.h"
 
 int main(){
-    
-    std::cout << "Hello World!" << std::endl;
 
+    Object dynamic_obj = Object(0,0,1,1,M_PI/2);
+    std::unique_ptr<coordinates[]> dynamic_pos = dynamic_obj.simulate_position(10, 1);
+    for (int i = 0; i < 11; i++){
+        std::cout << dynamic_pos[i] << std::endl;
+    }
     return 0;
 }
