032bec1d1ee8fb715f9d9b7c6abd92e13b4468ff
andejerv <jervananders@outlook.com>
2024-04-18 16:41:59 +0200

    Add main

--- sb_mpc.cpp
+++ sb_mpc.cpp
@@ -1,1 +1,9 @@
 #include "sb_mpc.h"
+
+SB_MPC::SB_MPC(){
+    // Constructor
+}
+
+SB_MPC::~SB_MPC(){
+    // Destructor
+}
--- sb_mpc.h
+++ sb_mpc.h
@@ -1,1 +1,11 @@
 #pragma once
+
+class SB_MPC{
+    
+    private:
+
+    public:
+        SB_MPC();
+        ~SB_MPC();
+        
+};
