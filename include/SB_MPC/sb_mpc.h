8c62425540263004eb55d65be299de6e2e01d882
andejerv <jervananders@outlook.com>
2024-04-19 17:18:08 +0200

    getBestControlOffset progress.

--- include/SB_MPC/sb_mpc.h
+++ include/SB_MPC/sb_mpc.h
@@ -13,10 +13,13 @@ class SB_MPC
         int num_steps = 0;
         double step_size = 0;
 
+        double hdg_offsets[5] = {-10, -5, 0, 5, 10};
+        //double vel_offsets[7] = {0.5, 1, 1.5, 2};
+
     public:
         SB_MPC(int n_steps, double step_sz);
         ~SB_MPC();
 
         void getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>* obstacles);
-        
+        double costFunction();
 };
--- src/sb_mpc.cpp
+++ src/sb_mpc.cpp
@@ -14,10 +14,23 @@ void SB_MPC::getBestControlOffset(double & hdg_os_best, double & vel_os_best, do
     vel_os_best = 1;
     int n_obst = obstacles->size();
 
-    // If there are no obstacles, return
+    // If there are no obstacles, return with no offset
     if(n_obst == 0){
         return;
     }
 
+    Eigen::MatrixXd sim_states;
+    // For all possible heading offsets
+    for (auto offset : hdg_offsets){
+        // Simulate the vehicle with the offset
+        sim_states = usv->simulate_positions(this->num_steps, this->step_size, Eigen::Vector2d(vel, hdg + offset));
+
+    }
+
+
     return;
+}
+
+double SB_MPC::costFunction(){
+    return 0;
 }
