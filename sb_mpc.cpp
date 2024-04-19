5a402c1b0234de8c02377f4fc4281e9bdec0fb85
andejerv <jervananders@outlook.com>
2024-04-19 14:11:54 +0200

    Started on getBestControlOffset.

--- sb_mpc.cpp
+++ sb_mpc.cpp
@@ -1,9 +1,23 @@
 #include "sb_mpc.h"
 
-SB_MPC::SB_MPC(){
+SB_MPC::SB_MPC(int n_steps, double step_sz): num_steps(n_steps), step_size(step_sz){
     // Constructor
 }
 
 SB_MPC::~SB_MPC(){
     // Destructor
+}
+
+void SB_MPC::getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>* obstacles){
+    double cost = INFINITY;
+    hdg_os_best = 0;
+    vel_os_best = 1;
+    int n_obst = obstacles->size();
+
+    // If there are no obstacles, return
+    if(n_obst == 0){
+        return;
+    }
+
+    return;
 }
--- sb_mpc.h
+++ sb_mpc.h
@@ -1,12 +1,22 @@
 #pragma once
 
+#include <vector>
+#include <eigen3/Eigen/Dense>
+
+#include "bb_model.h"
+#include "object.h"
+
 class SB_MPC
 {
 
     private:
+        int num_steps = 0;
+        double step_size = 0;
 
     public:
-        SB_MPC();
+        SB_MPC(int n_steps, double step_sz);
         ~SB_MPC();
+
+        void getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>* obstacles);
         
 };
