f98db6f1c2c91e0cee7724367d7337aafbd13055
andejerv <jervananders@outlook.com>
2024-04-19 02:27:21 +0200

    Object and agent test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <eigen3/Eigen/Dense>
 
+#include "bb_model.h"
 #include "object.h"
 
 int main(){
@@ -10,6 +12,20 @@ int main(){
     Eigen::MatrixXd dynamic_pos = dynamic_obj.simulate_position(10, 1);
 
     std::cout << dynamic_pos << std::endl;
+
+    BB_MODEL bb_model = BB_MODEL();
+
+    Eigen::VectorXd state(4);
+    state << 0, 0, 1, 0; // N, E, Velocity, Heading
+
+    Eigen::VectorXd control_input(2);
+    control_input << 1, 0; // Acceleration, Heading
+
+    Eigen::MatrixXd positions = bb_model.simulate_positions(10, 1, state, control_input);
+
+    std::cout << positions << std::endl;
+    
+
 
     return 0;
 }
