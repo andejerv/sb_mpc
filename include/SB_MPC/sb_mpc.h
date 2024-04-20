66e45d915bc26e0943207e9426717add62935620
andejerv <jervananders@outlook.com>
2024-04-20 17:53:33 +0200

    Collision risk is complete, needs testing.

--- include/SB_MPC/sb_mpc.h
+++ include/SB_MPC/sb_mpc.h
@@ -16,10 +16,18 @@ class SB_MPC
         double hdg_offsets[5] = {-10, -5, 0, 5, 10};
         //double vel_offsets[7] = {0.5, 1, 1.5, 2};
 
+        // Saftey parameters
+        double d_safe = 1.0; // distance below is considerd a collision
+        double d_close = 0.5; // distance for which an object are considered in the algorithm
+        double time_to_collision_weigth = 1.0;
+        double distance_within_dsafe_weigth = 1.0;
+
+
     public:
         SB_MPC(int n_steps, double step_sz);
         ~SB_MPC();
 
-        void getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>* obstacles);
-        double costFunction();
+        void getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>& obstacles);
+        double costFunction(const Eigen::MatrixXd & sim_states, const std::vector<Eigen::MatrixXd> & obst_positions, std::vector<Object*>& obstacles);
+        double collisionRisk(int k, double dist, Object * obj);
 };
--- src/sb_mpc.cpp
+++ src/sb_mpc.cpp
@@ -8,29 +8,83 @@ SB_MPC::~SB_MPC(){
     // Destructor
 }
 
-void SB_MPC::getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>* obstacles){
+void SB_MPC::getBestControlOffset(double & hdg_os_best, double & vel_os_best, double hdg, double vel, BB_MODEL * usv, std::vector<Object*>& obstacles){
     double cost = INFINITY;
     hdg_os_best = 0;
     vel_os_best = 1;
-    int n_obst = obstacles->size();
+    double cost_i = 0;
+    int n_obst = obstacles.size();
+
+    std::vector<double> costs;
 
     // If there are no obstacles, return with no offset
     if(n_obst == 0){
         return;
     }
 
+    std::cout << "Detected " << n_obst << " obstacle(s)." << std::endl;
+
+    // Create a vector of Eigen::MatrixXd to store the positions of the obstacles
+    std::vector<Eigen::MatrixXd> obst_positions;
+    for (int i = 0; i < n_obst; i++){
+        obst_positions.push_back(obstacles.at(i)->simulate_positions(this->num_steps, this->step_size));
+    }
+
     Eigen::MatrixXd sim_states;
     // For all possible heading offsets
     for (auto offset : hdg_offsets){
         // Simulate the vehicle with the offset
+        std::cout << "Checking offset: " << offset << std::endl << std::endl;
         sim_states = usv->simulate_positions(this->num_steps, this->step_size, Eigen::Vector2d(vel, hdg + offset));
-
+        std::cout << "Simulated states: " << std::endl << sim_states << std::endl << std::endl;
+        cost_i = costFunction(sim_states, obst_positions, obstacles);
+        costs.push_back(cost_i);
     }
 
+    // Find lowest cost in cost vector
+    for(int i = 0; i < costs.size(); i++){
+        std::cout << "Cost: " << costs.at(i) << std::endl;
+        if(costs.at(i) < cost){
+            cost = costs.at(i);
+            hdg_os_best = this->hdg_offsets[i];
+        }
+    }
 
     return;
 }
 
-double SB_MPC::costFunction(){
-    return 0;
+double SB_MPC::costFunction(const Eigen::MatrixXd & sim_states, const std::vector<Eigen::MatrixXd> & obst_positions, std::vector<Object*>& obstacles){
+    double cost = 0;
+
+    // First two states should be (x,y) position in grid
+    Eigen::MatrixXd sim_states_pos = sim_states.block(0, 0, 2, sim_states.cols());
+
+    // For every obstacle
+    for(int i = 0; i < obst_positions.size(); i++){
+        Eigen::MatrixXd dist = obst_positions.at(i) - sim_states;
+        std::cout << "Distance: " << std::endl << dist << std::endl << std::endl;
+        
+
+        for(int k = 0; k < dist.cols(); k++){
+            cost += collisionRisk(k, dist.col(k).norm(), obstacles.at(i));
+        }
+    }
+
+    return cost;
+}
+
+double SB_MPC::collisionRisk(int k, double dist, Object * obj){
+    if(dist > obj->getSafeDistance()){
+        return 0;
+    }
+
+    if(dist < obj->getCollisionDistance()){
+        // FIXME This should be size * abs(speed difference vector)²
+        return obj->getSize();
+    }
+
+    double time_risk = 1/(std::pow(k*this->step_size, this->time_to_collision_weigth));
+    double distance_risk = std::pow(obj->getSafeDistance()/dist, this->distance_within_dsafe_weigth);
+
+    return time_risk * distance_risk;
 }
